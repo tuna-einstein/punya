4df831bcc4c93349fb405eb0851c3691d8f3a13f
Uma Sankar Pradhan <umasankar@umasankar-macbookpro2.roam.corp.google.com>
2016-06-05 12:10:27 -0700

    Add auth token fetch function

--- webapp/js/endpoint.js
+++ webapp/js/endpoint.js
@@ -2,7 +2,7 @@
 var usp = usp || {};
 usp.punya = usp.punya || {};
 usp.punya.CLIENT_ID =
-	'replace this with your web application client ID';
+	'816356369753-2fk8iao9pvdqbl4inor9qanfuuuimcbp.apps.googleusercontent.com';
 
 usp.punya.SCOPES =
 	'https://www.googleapis.com/auth/userinfo.email';
@@ -36,7 +36,7 @@ usp.punya.loadData = function() {
 					listBooks.reject(resp);
 				}
 			});
-	
+
 	$.when(listBooks).done(function(result1) {
 		usp.punya.BOOKS.resolve(result1.items || []);
 		//console.log(result1);
@@ -46,22 +46,72 @@ usp.punya.loadData = function() {
 	});
 };
 
+
+function prms_gapi_load_by(api,v, apiRoot) {
+	var df = $.Deferred();
+	// gapi.client.setApiKey(constants.gapi.api_key);
+	if (apiRoot) {
+		gapi.client.load(api, v, function(){
+			console.log("load success:" + api);
+			df.resolve("loaded:" + api);
+		}, apiRoot);
+	} else {
+		gapi.client.load(api, v, function(){
+			console.log("load success:" + api);
+			df.resolve("loaded:" + api);
+		});
+	}
+	return df.promise();
+}
+
+function prms_gapi_auth(mode){
+	var df = $.Deferred();
+	gapi.auth.authorize(
+			{
+				client_id: usp.punya.CLIENT_ID, 
+				scope: usp.punya.SCOPES, immediate: mode
+			},
+			function(res) {
+				if(!gapi.auth || !gapi.auth.getToken()){
+					df.reject("gapi.auth not exists");
+				}
+
+				if (res && !res.error) {
+					console.log("auth success");
+					df.resolve(res);
+				} else {
+					console.log("auth failed..retry");
+					df.reject(res);
+				}
+			});
+	return df.promise();
+}
+
+
+usp.punya.loadApis = function(apiRoot) {
+	$.when(prms_gapi_load_by('bookendpoint', 'v1', apiRoot),
+			prms_gapi_load_by('oauth2', 'v2')).done(function(bookendpoint, oauth2) {
+				usp.punya.loadData();
+			}).fail(function(failResult) {
+				console.log(failResult)
+			}); 
+}
 /**
  * Initializes the application.
  * @param {string} apiRoot Root of the API's path.
  */
 usp.punya.init = function(apiRoot) {
-	// Loads the OAuth and helloworld APIs asynchronously, and triggers login
-	// when they have completed.
-	var apisToLoad;
-	var callback = function() {
-		if (--apisToLoad == 0) {
-			usp.punya.loadData();
-		}
-		console.log(apisToLoad);
-	}
-
-	apisToLoad = 2; // must match number of calls to gapi.client.load()
-	gapi.client.load('bookendpoint', 'v1', callback, apiRoot);
-	gapi.client.load('oauth2', 'v2', callback);
+	// Ask for immediate auth_token. If fails, show user consent screen.
+	prms_gapi_auth(true).done(
+			function(res) {
+				console.log("auth callback");
+				usp.punya.loadApis(apiRoot);
+			}).fail(function(res) {
+				// show user consent screen.
+				prms_gapi_auth(false).done(
+						function(res) {
+							console.log("auth callback on userconsent");
+							usp.punya.loadApis(apiRoot);
+						});
+			});
 };
